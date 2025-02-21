bf1cdecfa4ffc27062da5b702ed8ab20d52fc695
Domjon24 <Domjon24@gmail.com>
2025-02-21 16:29:42 -0600

    done for the day. still need to finish initial bootstrap styling

--- chart1.js
+++ chart1.js
@@ -1,23 +1,23 @@
-const ctx = document.getElementById('myChart');
+// const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2],
-      borderWidth: 0.3
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+// new Chart(ctx, {
+//   type: 'doughnut',
+//   data: {
+//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+//     datasets: [{
+//       label: '# of Votes',
+//       data: [12, 19, 3, 5, 2],
+//       borderWidth: 0.3
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true
+//       }
+//     }
+//   }
+// });
 
 
 
