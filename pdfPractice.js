6d583ecb817148ecd68eab142c0ffab23dc7f944
Dominique <domjon24@gmail.com>
2025-02-27 01:28:41 -0600

    added an upload button. thinking of adding file upload directly in table. not sure how to push values into variable if the PDF parse needs to be within async function

--- pdfPractice.js
+++ pdfPractice.js
@@ -2,45 +2,54 @@ const input = document.getElementById('pdf-upload');
 const output = document.getElementById('pdf-content');
 
 
-
 input.addEventListener('change', async () => {
     // const file = input.files[0];
     const files = input.files;
-    let allText = "";
-    
+    let allText = [];
+
     for (let file of files){
-    if (file) {
-        const fileReader = new FileReader();
+        if (file) {
+            const fileReader = new FileReader();
 
-        fileReader.onload = async function() {
-            try {
-                const typedArray = new Uint8Array(this.result);
-                const pdf = await pdfjsLib.getDocument(typedArray).promise;
-                const numPages = pdf.numPages;
-                
-                let thisText = "";
-
-                for (let i = 1; i <= numPages; i++) {
+            fileReader.onload = async function() {
+                try {
+                    const typedArray = new Uint8Array(this.result);
+                    const pdf = await pdfjsLib.getDocument(typedArray).promise;
+                    const numPages = pdf.numPages;
                     
-                    const page = await pdf.getPage(i);
-                    const textContent = await page.getTextContent();
-                    const pageText = textContent.items.map(item => item.str).join(" ");
-                    thisText += pageText.toString() + "\n";
+                    let thisText = "";
 
-                    thisIndex = thisText.search('DOMINIQUE');
+                    for (let i = 1; i <= numPages; i++) {
+                        
+                        const page = await pdf.getPage(i);
+                        const textContent = await page.getTextContent();
+                        const pageText = textContent.items.map(item => item.str).join(" ");
+                        thisText += pageText.toString();
 
-                    // console.log(pageText)
-                    console.log("index " + thisIndex)
+                        thisIndex = thisText.search('DOMINIQUE');
+                        // console.log(pageText)
+                        console.log("index " + thisIndex)
+                    }
+                        allText.push(thisText);
+                        // output.textContent = allText.join('');
+                        console.log(allText)
+                        console.log(allText.length)
+                    
+                } catch (error) {
+                    output.textContent = "Error parsing PDF: " + error.message;
                 }
-                allText += thisText;
-                output.textContent = allText;
-                
-            } catch (error) {
-                output.textContent = "Error parsing PDF: " + error.message;
-            }
-        };
-        fileReader.readAsArrayBuffer(file);
+            };
+              fileReader.readAsArrayBuffer(file);
+        }
     }
-}
 })
 
+const myForm = document.getElementById('pdfForm');
+const inpFile = document.getElementById('pdf-upload');
+
+    myForm.addEventListener('submit', e => {
+        e.preventDefault();
+            let area = document.getElementById('pdf-content')
+            console.log(inpFile)
+    });
+    
