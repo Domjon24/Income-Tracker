4f53e9f6296151e31aa5263f1006f11e00c0f4da
Domjon24 <Domjon24@gmail.com>
2025-03-03 01:24:10 -0600

    biggest commit yet! replaced preview func with create card function. it gives a preview of all the file data uploaded before submitting. resets upon every submit & file upload. Will focus on formatting next

--- pdfPractice.js
+++ pdfPractice.js
@@ -5,85 +5,102 @@ let fileCount = 0;
 let allPDFText = [];
 let fileName = [];
 
-
-function convertDateFormat (date){ //converts date to yyyy-mm-dd so form can accept it
-    let year = date.slice(6,11) + "-";
-    let month = date.slice(0,2) + "-";
-    let day = date.slice(3,5);
+function convertDateFormat (date){  //converts date to yyyy-mm-dd so form can accept it
+    let year = date.slice(6, 11) + "-";
+    let month = date.slice(0, 2) + "-";
+    let day = date.slice(3, 5);
     return year + month + day;
 }
 
-function findValues (payStartingIndex, dateStartingIndex, hoursStartingIndex, parsedPDFText){ //finds values, formats & pushes into sub-array within allPDFText arr
-        let values = [];
-        let a = payStartingIndex-8;
-        let b = dateStartingIndex + 19;
-        let c = dateStartingIndex + 30;
-        let d = hoursStartingIndex + 10
-        let e = hoursStartingIndex + 23
-        values.push(Math.round(parsedPDFText.slice(a, payStartingIndex).trim())); //net pay
-        values.push(convertDateFormat(parsedPDFText.slice(b, c).trim()));//date
-        values.push(Math.round(parsedPDFText.slice(d, e).trim())); //hours
-        allPDFText.push(values)
-            //finds all 3 values, formats them & pushes into addPDFText array
-}
-function resetInputValues(){
-    document.getElementById('pdf-upload').value = null;
-    allPDFText.length = 0
+function findValues (payStartingIndex, dateStartingIndex, hoursStartingIndex, parsedPDFText){ 
+    let values = [];
+    let a = payStartingIndex - 8;
+    let b = dateStartingIndex + 19;
+    let c = dateStartingIndex + 30;
+    let d = hoursStartingIndex + 10;
+    let e = hoursStartingIndex + 23;
+    values.push(Math.round(parsedPDFText.slice(a, payStartingIndex).trim())); //net pay
+    values.push(convertDateFormat(parsedPDFText.slice(b, c).trim()));//date
+    values.push(Math.round(parsedPDFText.slice(d, e).trim())); //hours
+    allPDFText.push(values);
+    return values; // finds & formats values, pushes into addPDFText, then returns values for card display
 }
 
-function previewFileValues(index){
-    output.textContent = ' '
-    console.log('File name:', index.name)
-    fileName.push(index.name)
-    output.textContent += fileName + '\n';
+function resetInputValues(){
+    allPDFText.length = 0;
+    document.getElementById('pdf-cards').innerHTML = '';
 }
+
+function createCard(fileName, hours, date, pay){
+    const cardContainer = document.getElementById('pdf-cards'); 
+    const card = document.createElement('div');
+
+    card.classList.add('card');
+    card.innerHTML = `
+        <h5 class="card-title">${fileName}</h5>
+        <p class="card-text">Hours Worked: ${hours}<br>
+            Date: ${date}<br>
+            Total Pay: ${pay}</p>
+
+    `;
+    cardContainer.appendChild(card);
+}
+
 async function parsePDF(){
     input.addEventListener('change', async () => {
-        allPDFText.length = 0 //resets selected file if it's changed before submitting to prevent duplicates
+        allPDFText.length = 0;//resets selected file if it's changed before submitting to prevent duplicates
         fileName.length = 0;
+
+        
+        const cardContainer = document.getElementById('pdf-cards');
+        cardContainer.innerHTML = '';  // Clears the old cards
+
         const files = input.files;
 
-        for (let file of files){ // loops through all input files and parses text
+        for (let file of files) {  // loops through all input files and parses text
 
-                // console.log('File name:', file.name);
-                previewFileValues(file)
-                const fileReader = new FileReader(); //new obj constructor
-                fileReader.onload = async function() {
-                    try {
+            // previewFileValues(file);
+            const fileReader = new FileReader();//new obj constructor
+            fileReader.onload = async function() {
+                try {
 
-                        const typedArray = new Uint8Array(this.result);
-                        const pdf = await pdfjsLib.getDocument(typedArray).promise;
-                        // const numPages = pdf.numPages; //keeping this handy for scaling reasons
-                        const page = await pdf.getPage(1);
-                        let thisText = "";  //each file gets all their text put into here before going into allPDFText Arr
-                        const textContent = await page.getTextContent();
-                        thisText += textContent.items.map(item => item.str).join(" ").toString();
+                    const typedArray = new Uint8Array(this.result);
+                    const pdf = await pdfjsLib.getDocument(typedArray).promise;
+                    // const numPages = pdf.numPages; //keeping this handy for scaling reasons
+                    const page = await pdf.getPage(1);
+                    let thisText = "";  //each file gets all their text put into here before going into allPDFText Arr
+                    const textContent = await page.getTextContent();
+                    thisText += textContent.items.map(item => item.str).join(" ").toString();
 
-                            if (thisText.search('GLENLAKE') < 1 || thisText.search('BFTAX') < 1 || thisText.search('Rate Miles') < 1) {
-                                showAlert2(`${document.getElementById('pdf-upload').value} is invalid, try again`, "danger")
-                                return false;  //conditional validates pdf & returns filepath that didn't upload. else, it runs
-                            }
-                               
-                                    thisIndex = thisText.search('Earnings Tax');
-                                    dateIndex = thisText.search('XXX');
-                                    hours = thisText.search('RS WORKED');
-
-                                    findValues(thisIndex, dateIndex, hours, thisText)
-                                
-                                    // output.textContent = allPDFText.join(''); //displays pdf values on page
-                                    
-                                    console.log(allPDFText)
-                                    
-                                
-                    } catch (error) {
-                        showAlert(`${error.message}`, "danger")
-                        output.textContent = "Error parsing PDF: " + error.message;
+                    if (thisText.search('GLENLAKE') < 1 || thisText.search('BFTAX') < 1 || thisText.search('Rate Miles') < 1) {
+                        showAlert2(`${document.getElementById('pdf-upload').value} is invalid, try again`, "danger");
+                        return false; //conditional validates pdf & returns filepath that didn't upload. else, it runs
                     }
-                };
-                fileReader.readAsArrayBuffer(file);  
+
+                    let thisIndex = thisText.search('Earnings Tax');
+                    let dateIndex = thisText.search('XXX');
+                    let hours = thisText.search('RS WORKED');
+
+                    let values = findValues(thisIndex, dateIndex, hours, thisText); //needs to be called here to make sure values are populated first
+                    let hoursWorked = values[2];
+                    let date = values[1];
+                    let pay = values[0] 
+                    createCard(file.name, hoursWorked, date, pay); // Create a card for each file showing each value
+                    // output.textContent = allPDFText.join(''); //displays pdf values on page
+                } catch (error) {
+                    showAlert(`${error.message}`, "danger");
+                    output.textContent = "Error parsing PDF: " + error.message;
+                }
+            };
+            fileReader.readAsArrayBuffer(file);
         } 
-        return;      
-    })
+    });
+}
+
+async function previewFileValues(file){
+    output.textContent = ' ';
+    console.log('File name:', file.name);
+    fileName.push(file.name);
 }
 
 /*Calling main PDF Function*/     parsePDF()
@@ -101,35 +118,34 @@ myForm.addEventListener('submit', e => { //submit button function
         //     console.log(`submit button pressed. Filename is: ${input.value} current file count is ${fileCount}`)
             console.log(`pdf array is ${allPDFText} length is ${allPDFText.length}`)
 
-            for (let i = 0; i < allPDFText.length; i++) {
+    for (let i = 0; i < allPDFText.length; i++) {
 
-                let paycheckDate = allPDFText[i][1];
-                let netPay = allPDFText[i][0];
-                let hoursWorked = allPDFText[i][2];
-                console.log("index is " + i)
-                console.log("file check " + allPDFText[i])
+        let paycheckDate = allPDFText[i][1];
+        let netPay = allPDFText[i][0];
+        let hoursWorked = allPDFText[i][2];
+        console.log("index is " + i)
+        console.log("file check " + allPDFText[i])
 
-                    if (selectedRow == null) {
-                        const list = document.querySelector("#groceryList");
-                        const row = document.createElement("tr");
-                        const yAxisDate = myChart1.data.datasets[0].data;
-                
-                        row.innerHTML = `
+        if (selectedRow == null) {
+            const list = document.querySelector("#groceryList");
+            const row = document.createElement("tr");
+            const yAxisDate = myChart1.data.datasets[0].data;
+            row.innerHTML = `
                         <td>${paycheckDate}</td>
                         <td>${netPay}</td>
                         <td>${hoursWorked}</td>
                         <td>
                         <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
                         <a href="#" class="btn btn-danger btn-sm delete">Delete</a>`;
-                
-                        list.appendChild(row);
-                        selectedRow = null;
-                        // console.log(yAxisDate);
-                        updateMyChart()
-                        
-                        showAlert("Item Added", "success");   
-                    }
-            
+            list.appendChild(row);
+            selectedRow = null;
+            // console.log(yAxisDate);
+            updateMyChart();
+            showAlert("Item Added", "success");
         }
-        resetInputValues()
-})
+    }
+    resetInputValues();
+});
+
+
+// parsePDF();
